134216eda4e5845db126aab376f565fffb6daa5b
Erik Telford <erik@microventures.com>
2018-11-16 08:43:21 -0600

    Add relationship

--- app/Investment.php
+++ app/Investment.php
@@ -6,4 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Investment extends Model
 {
+    /**
+     * An investment belongs to an Investor
+     *
+     * @return BelongsTo
+     */
+    public function investor()
+    {
+        return $this->belongsTo(Investor::class);
+    }
 }
